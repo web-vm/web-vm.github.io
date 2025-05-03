13868abcbaa4691d15e198b8003f48ef81676b8c
web-vm <pyvendingmachines@gmail.com>
2025-05-04 00:24:21 +0530

    Fourth commit vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,14 +5,14 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/web-vm.github.io/",  // 👈 REQUIRED for GitHub Pages
   server: {
     host: "::",
     port: 8080,
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === "development" && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
